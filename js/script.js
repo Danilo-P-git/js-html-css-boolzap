d36c6b1c633a033f9928b7d1990f77c79918c3a3
Danilo Patanè <d.patane98@gmail.com>
2020-09-09 14:56:29 +0200

    Answer back done

--- js/script.js
+++ js/script.js
@@ -3,22 +3,70 @@ $(document).ready(function() {
   $("#msg-input").keypress(function(e) {
     if (e.which == 13) {
       var valoreInserito = $(this).val();
-      var messaggioInterno = $(".msg-structure .msg-row").clone();
+      var messaggioInterno = $(".msg-structure .user").clone();
+      var date = new Date();
+      var hours = date.getHours();
+      var minutes = date.getMinutes();
+      var time = hours + ":" + minutes;
       if (valoreInserito !="") {
         messaggioInterno.find(".testo-msg").text(valoreInserito);
+        messaggioInterno.find(".ora-msg").text(time);
         $(".main-chat").append(messaggioInterno);
         $(this).val("");
       }
+      setTimeout(function () {
+        var valoreInserito = "OK";
+        var messaggioInterno = $(".msg-structure .cpu").clone();
+        var date = new Date();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var time = hours + ":" + minutes;
+        if (valoreInserito !="") {
+          messaggioInterno.find(".testo-msg").text(valoreInserito);
+            messaggioInterno.find(".ora-msg").text(time);
+          $(".main-chat").append(messaggioInterno);
+          $("#msg-input").val("");
+        };
+      }, 2000);
     }
+
     });
 
     $("#invia").click(function(){
-      var valoreInserito = $("#msg-input").val();
-      var messaggioInterno = $(".msg-structure .msg-row").clone();
-      if (valoreInserito !="") {
-        messaggioInterno.find(".testo-msg").text(valoreInserito);
-        $(".main-chat").append(messaggioInterno);
-        $("#msg-input").val("");
-      };
+      messaggioInviato()
+      setTimeout(function () {
+        var valoreInserito = "OK";
+        var messaggioInterno = $(".msg-structure .cpu").clone();
+        var date = new Date();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var time = hours + ":" + minutes;
+        if (valoreInserito !="") {
+          messaggioInterno.find(".testo-msg").text(valoreInserito);
+            messaggioInterno.find(".ora-msg").text(time);
+          $(".main-chat").append(messaggioInterno);
+          $("#msg-input").val("");
+        };
+      }, 2000);
   });
+
+
+function messaggioInviato() {
+  var valoreInserito = $("#msg-input").val();
+  var messaggioInterno = $(".msg-structure .user").clone();
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var time = hours + ":" + minutes;
+  if (valoreInserito !="") {
+    messaggioInterno.find(".testo-msg").text(valoreInserito);
+      messaggioInterno.find(".ora-msg").text(time);
+    $(".main-chat").append(messaggioInterno);
+    $("#msg-input").val("");
+  };
+}
+
+
+
+
 });
